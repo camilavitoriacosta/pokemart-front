d3c37cd50a1974e9be1b43136005bc42759fe18e
camilacosta <camilavitoria.c.costa@gmail.com>
2023-02-10 17:20:31 -0400

    configuração projeto + navbar

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="d-flex text-center fill-height">
+      <v-item-group multiple>
+        <v-container>
+          <v-row>
+            <v-col v-for="pokemon in pokemons" :key="pokemon.nome" cols="12" md="4">
+              <v-item v-slot="{ isSelected, toggle }">
+                <v-card :color="isSelected ? 'purple' : ''" class="d-flex align-center" dark height="200"
+                  @click="toggle">
+                  <v-scroll-y-transition>
+                    <div class="text-h3 flex-grow-1 text-center">
+                      {{ isSelected? pokemon.numeroPokedex : pokemon.nome }}
+                    </div>
+                  </v-scroll-y-transition>
+                </v-card>
+              </v-item>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-item-group>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script >
+export default {
+  data() {
+    return {
+      pokemons: [
+        {
+          nome: "Pikachu",
+          nivel: "25",
+          numeroPokedex: "26"
+        },
+        {
+          nome: "Charmander",
+          nivel: "25",
+          numeroPokedex: "4"
+        },
+        {
+          nome: "Squirte",
+          nivel: "25",
+          numeroPokedex: "7"
+        },
+        {
+          nome: "Bulbassauro",
+          nivel: "25",
+          numeroPokedex: "1"
+        },
+      ]
+    }
+  }
+}
+</script>
---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app-bar prominent color="purple">
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    </template>
+    <v-app-bar-title>
+      Pokemart
+    </v-app-bar-title>
+  </v-app-bar>
+
+  <v-navigation-drawer v-model="drawer" location="top" temporary>
+    <v-list density="comfortable" :items="menus">
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      menus:[
+        {
+          title: "Pokemons",
+          value: "/pokemons"
+        }
+      ]
+    }
+  }
+}
+</script>
